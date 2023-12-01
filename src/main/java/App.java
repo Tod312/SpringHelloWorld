167dffe42e623d9368abb89ecb0988c11258ec85
Seren9 <shashkov_serega@mail.ru>
2023-12-01 11:10:04 +0300

    Add bean and class Cat

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,18 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
+        
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 = applicationContext.getBean("helloworld", HelloWorld.class);
         System.out.println(bean.getMessage());
+
+        Cat catBean = applicationContext.getBean("cat", Cat.class);
+        Cat catBean2 = applicationContext.getBean("cat", Cat.class);
+        System.out.println(catBean.getMessage());
+        
+        System.out.println("Is this the same HelloWorldbean? -" + (bean == bean2));
+        System.out.println("Is this the same cat? -" + (catBean == catBean2));
+        
+        
     }
 }
---
+++ src/main/java/Cat.java
@@ -0,0 +1,19 @@
+
+public class Cat {
+	private String message;
+
+	/**
+	 * @return the message
+	 */
+	public String getMessage() {
+		return message;
+	}
+
+	/**
+	 * @param message the message to set
+	 */
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+}
